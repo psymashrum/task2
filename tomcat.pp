f1af950f966a1b800be971d64b7b2410f3a70f7e
Alex Elkin <alex.elkin.name@gmail.com>
2016-10-04 21:47:01 +0600

    35%

--- tomcat.pp
+++ tomcat.pp
@@ -9,11 +9,5 @@ service { "tomcat":
 
 file { "/opt/tomcat/conf/server.xml":
   require=> Package["tomcat"],
-  content => 
-      "
-      <Connector address="127.0.0.1" port="8080" protocol="HTTP/1.1" 
-      connectionTimeout="20000" 
-      redirectPort="8888" />
-      }",
   notify => Service["tomcat"]
 }
